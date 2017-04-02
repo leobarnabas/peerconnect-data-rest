ebf4ef96a9c229de0541d83236565b8be1e4aaf7
leobarnabas <leobarnabas@gmail.com>
2017-04-02 14:49:16 -0400

    Adding swagget ui, gitignore

--- src/main/java/org/pcg/Application.java
+++ src/main/java/org/pcg/Application.java
@@ -3,11 +3,42 @@ package org.pcg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("")
+				.apiInfo(apiInfo())
+				.select()
+				.paths(regex("/.*"))
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Spring REST Mongo Sample with Swagger")
+				.description("Spring REST Mongo Sample with Swagger")
+				.termsOfServiceUrl("")
+				.contact("Leo")
+				.license("Apache License Version 2.0")
+				.licenseUrl("")
+				.version("2.0")
+				.build();
 	}
 }
